3e3c9c23c18b761277c767dba432f3c83ea10876
Nikita <pencilprn@mail.ru>
2026-01-13 14:44:18 +0300

    fix sslmode

--- internal/bootstrap/pgstorage.go
+++ internal/bootstrap/pgstorage.go
@@ -10,8 +10,8 @@ import (
 
 func InitPGStorage(cfg *config.Config) *pgstorage.PGstorage {
 
-	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
-		cfg.Database.Username, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.DBName)
+	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		cfg.Database.Username, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.DBName, cfg.Database.SSLMode)
 	storage, err := pgstorage.NewPGStorage(connectionString)
 	if err != nil {
 		log.Panicf("ошибка инициализации БД, %v", err)
